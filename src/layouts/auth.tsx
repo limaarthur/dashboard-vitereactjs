052d75e06ec867fdfb519a04641627a83feaaa78
limaarthur <artglima.dev@gmail.com>
2026-03-10 12:55:07 -0300

    feat: create login page

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -1,11 +1,20 @@
+import { LayoutDashboard } from 'lucide-react'
 import { Outlet } from 'react-router'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg">
+          <LayoutDashboard className="h-5 w-5" />
+          <span className="font-semibold">dashboard.web</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; dashboard.web - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
