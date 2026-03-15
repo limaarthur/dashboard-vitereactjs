1948bbd0269da6c175d76c268fbcee4762fb5db8
limaarthur <artglima.dev@gmail.com>
2026-03-15 20:32:11 -0300

    feat: create themetoggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
-
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './router'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | dashboard.web" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider
+        storageKey="dashboardweb-theme"
+        defaultTheme="dark"
+      >
+        <Helmet titleTemplate="%s | dashboard.web" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
