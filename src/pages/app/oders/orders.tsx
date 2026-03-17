f0fd86a94fcf669e4d6748b996f3549bee6c56f6
limaarthur <artglima.dev@gmail.com>
2026-03-17 14:08:22 -0300

    feat: create order filters component

--- src/pages/app/oders/orders.tsx
+++ src/pages/app/oders/orders.tsx
@@ -1,16 +1,14 @@
-import { ArrowRight, Search, X } from 'lucide-react'
-
 import { PageTitle } from '@/components/page-title'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
   return (
@@ -20,13 +18,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input
-            placeholder="Nome do cliente"
-            className="h-8 w-[320px]"
-          />
-        </form>
+        <OrderTableFilters />
 
         <div className="rounded-md border">
           <Table>
@@ -50,48 +42,7 @@ export function Orders() {
             </TableHeader>
             <TableBody>
               {Array.from({ length: 10 }).map((_, i) => (
-                <TableRow key={i}>
-                  <TableCell>
-                    <Button variant="outline" size="xs">
-                      <Search className="h-3 w-3" />
-                      <span className="sr-only">
-                        Detalhes do pedido
-                      </span>
-                    </Button>
-                  </TableCell>
-                  <TableCell className="font-mono text-xs font-medium">
-                    90s8d9sa8dDsa90
-                  </TableCell>
-                  <TableCell className="text-muted-foreground">
-                    há 15 minutos
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      <span className="h-2 w-2 rounded-full bg-slate-400" />
-                      <span className="text-muted-foreground font-medium">
-                        Pendente
-                      </span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="font-medium">
-                    Arthur Lima
-                  </TableCell>
-                  <TableCell className="font-medium">
-                    R$ 149,90
-                  </TableCell>
-                  <TableCell>
-                    <Button variant="outline" size="xs">
-                      <ArrowRight className="mr-2 h-3 w-3" />
-                      Aprovar
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant="ghost" size="xs">
-                      <X className="mr-2 h-3 w-3" />
-                      Cancelar
-                    </Button>
-                  </TableCell>
-                </TableRow>
+                <OrderTableRow key={i} />
               ))}
             </TableBody>
           </Table>
